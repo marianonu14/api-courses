c0c353a26e9f7b3685995fb585a495d321303769
Mariano <marianonunezma@gmail.com>
2022-10-10 15:37:49 -0300

    Adding HTTP Request Courses

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const courses = require('./data/courses.json');
 
-const technologies = require('./routes/technologies')
-const sports = require('./routes/sports')
+const app = express();
 
-const courses = require('./data/courses.json')
-
-app.get('/', function (req, res) {
-    res.send('Welcome to the API Courses...')
-    })
+app.use(express.static('build'))
 
 app.get('/api', (req, res) => {
     res.send(courses);
-    })
+})
 
+const technologies = require('./routes/technologies');
 app.use('/api/technologies', technologies);
+
+const sports = require('./routes/sports');
 app.use('/api/sports', sports);
-
-
 
 app.use((req, res) => {
     res.status(404).send('Page Not Found: Error 404')
-    })
+})
 
 const PORT = process.env.PORT || 3500
 
 app.listen(PORT, () => {
     console.log(`Listen on Port: ${PORT}`)
-    })
+})
--- routes/technologies.js
+++ routes/technologies.js
@@ -1,19 +1,19 @@
 const express = require('express'); 
 const router = express.Router();
 
-const courses = require('../data/courses.json');
+const courses = require('../data/courses.json').technologies;
 
 router.get('/', (req, res) => {
-    res.send(courses.technologies);
-    })
+    res.send(courses);
+})
     
 router.get('/:id', (req, res) => {
     const id = Number(req.params.id);
-    const result = courses.technologies.filter(course => course.id === id);
+    const result = courses.filter(course => course.id === id);
 
     if(!result.length) return res.send('ID not Founded')
 
     res.send(result);
-    })
+})
 
 module.exports = router;

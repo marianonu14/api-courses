a00847d558f9f1e53a97de7e9f64f61b04e2eb39
Mariano <marianonunezma@gmail.com>
2022-10-10 15:43:08 -0300

    Fixing HTTP Request

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const courses = require('./data/courses.json');
 
 const app = express();
 
-app.use(express.static('build'))
+app.use(express.static('build'));
 
 app.get('/api', (req, res) => {
-    res.send(courses);
-})
+  res.send(courses);
+});
 
 const technologies = require('./routes/technologies');
 app.use('/api/technologies', technologies);
@@ -17,11 +17,11 @@ const sports = require('./routes/sports');
 app.use('/api/sports', sports);
 
 app.use((req, res) => {
-    res.status(404).send('Page Not Found: Error 404')
-})
+  res.status(404).send('Page Not Found: Error 404');
+});
 
-const PORT = process.env.PORT || 3500
+const PORT = process.env.PORT || 3500;
 
 app.listen(PORT, () => {
-    console.log(`Listen on Port: ${PORT}`)
-})
+  console.log(`Listen on Port: ${PORT}`);
+});
--- routes/technologies.js
+++ routes/technologies.js
@@ -1,19 +1,49 @@
-const express = require('express'); 
+const express = require('express');
 const router = express.Router();
 
 const courses = require('../data/courses.json').technologies;
 
+router.use(express.json());
+
 router.get('/', (req, res) => {
-    res.send(courses);
-})
-    
+  res.send(courses);
+});
+
 router.get('/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const result = courses.filter(course => course.id === id);
+  const id = Number(req.params.id);
+  const result = courses.filter((course) => course.id === id);
 
-    if(!result.length) return res.send('ID not Founded')
+  if (!result.length) return res.send('ID not Founded');
 
-    res.send(result);
-})
+  res.send(result);
+});
+
+router.post('/', (req, res) => {
+  courses.push(req.body);
+
+  res.send(courses);
+});
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const index = courses.findIndex((course) => course.id === Number(id));
+
+  if (index === -1) return res.status(404).send('This course doesnt exist');
+
+  courses[index] = req.body;
+
+  res.send(courses);
+});
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  const index = courses.findIndex((course) => course.id === Number(id));
+
+  if (index === -1) return res.status(404).send('This course doesnt exist');
+
+  courses.splice(index, 1);
+
+  res.send(courses);
+});
 
 module.exports = router;
